32728eff4034f3fb5ff1c588915504cdf635352e
Elias Clark <eclark@twilio.com>
2022-05-31 11:54:08 -0400

    Basic Paste styling added

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Button } from "@twilio-paste/core/button";
+import { OrderedList, UnorderedList, ListItem, Badge } from "@twilio-paste/core";
+import {WarningIcon} from '@twilio-paste/icons/esm/WarningIcon'
 
 const baseURL = "http://localhost:3000/data";
 
@@ -16,8 +19,28 @@ export default function DataGet() {
 
   return (
     <div>
-      {todos.map((todo) => 
-      <li key={todo.id}> {todo.name} - {todo.due} - {todo.priority} </li> )}
+      
+      <UnorderedList>
+        {todos.map((todo) => (
+          <ListItem key={todo.id}>
+            {todo.name} - {todo.due} - 
+            <Badge as="span" variant="neutral"><WarningIcon decorative={false} title="Priority" />{todo.priority}</Badge>
+          </ListItem>
+        ))}
+      </UnorderedList>
+      <Button variant="secondary"> Update</Button>
     </div>
   );
 }
+
+// export default function DataPost() {
+//     // TODO: POST function w/ axios
+// }s
+
+// export default function DataUpdate() {
+//     //TODO: PUSH function w/ axios
+// }
+
+// export default function DataDelete() {
+//     //TODO: DELETE function w/ axios
+// }
---
+++ src/components/TopHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading } from "@twilio-paste/core/heading";
+
+export default function TopHeader() {
+  return (
+    
+      <Heading as="h1" variant="heading10">
+        Elias's cool To-Do project
+      </Heading>
+    
+  );
+}
