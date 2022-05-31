63d86b3d3fb13c3125074fa3c8eccd911813f468
Elias Clark <eclark@twilio.com>
2022-05-31 17:48:48 -0400

    Fixed up some more Paste styling

--- src/components/Data.js
+++ src/components/Data.js
@@ -6,7 +6,9 @@ import { WarningIcon } from "@twilio-paste/icons/esm/WarningIcon";
 import { Card } from "@twilio-paste/core";
 import { Heading } from "@twilio-paste/core";
 import { HistoryIcon } from "@twilio-paste/icons/esm/HistoryIcon";
+import {CheckboxCheckIcon } from "@twilio-paste/icons/esm/CheckboxCheckIcon";
 import { Box, Text, Separator } from "@twilio-paste/core";
+import { ChevronDoubleRightIcon } from "@twilio-paste/icons/esm/ChevronDoubleRightIcon";
 import Data from "./Data.css";
 
 const baseURL = "http://localhost:3000/data";
@@ -21,6 +23,31 @@ export default function DataGet() {
   }, []);
 
   if (!todos) return null;
+  
+  function PriorityGet(todo) {
+    if (todo.priority === "critical") {
+        return "warning";
+    } else if (todo.priority === "medium") {
+        return "decorative40"
+    } else { return "decorative20"}
+  };
+
+  function ProgressGet(todo) {
+      if (todo.progress ==="completed" ) {
+          return "decorative30"
+      } else if (todo.progress === "in progress"){
+          return "decorative40"
+      } else {return "decorative10"}
+  };
+
+  function ProgressGetIcon(todo){
+      if (todo.progress === "completed") {
+          return <CheckboxCheckIcon decorative={false} title="Progress" />
+      } else if (todo.progress === "in progress") {
+          return <ChevronDoubleRightIcon decorative={false} title="Progress" />
+      }
+      else { return <HistoryIcon decorative={false} title="Progress" />}
+  };
 
   return (
     <div>
@@ -28,14 +55,14 @@ export default function DataGet() {
         <div id="todoBox" key={todo.id}>
 
 
-          <Heading as="h2" variant="heading70"  >
-            TODO: {todo.name}
+          <Heading as="h2" variant="heading30"  >
+            <span style={{color: "rgb(174, 178, 193)"}}>TODO •</span> &nbsp;{todo.name}
           </Heading>
 
 
           <Box
             backgroundColor="colorBackgroundBody"
-            padding="space50"
+            padding="space30"
             display="flex"
             border="colorBorderDecorative10Weaker"
             justifyContent="space-between"
@@ -43,19 +70,19 @@ export default function DataGet() {
           >
             <Text as="span" id="dueDate">DUE: {todo.due}</Text>
             <Separator orientation="vertical" horizontalSpacing="space40" />
-            <Badge as="span" variant="neutral">
+            <Badge as="span" variant={PriorityGet(todo)}>
               <WarningIcon decorative={false} title="Priority" />
               &nbsp; {todo.priority}
             </Badge>
             <Separator orientation="vertical" horizontalSpacing="space40" />
-            <Badge as="span" variant="neutral">
-              <HistoryIcon decorative={false} title="Progress" />
+            <Badge as="span" variant={ProgressGet(todo)}>
+              {ProgressGetIcon(todo)}
               &nbsp; {todo.progress}
             </Badge>
             <Separator orientation="vertical" horizontalSpacing="space40" />
-            <Button variant="secondary"> Update Todo</Button>
+            <Button variant="secondary"> Update Task</Button>
             <Separator orientation="vertical" horizontalSpacing="space40" />
-            <Button variant="secondary"> Another One</Button>
+            <Button variant="destructive_secondary"> Delete Task</Button>
           </Box>
         </div>
       ))}
